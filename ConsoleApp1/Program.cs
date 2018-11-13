12e369039afe267ebb9c32d44c8e1c688ed80853
yanis <yanis.abdallah@insa-cvl.fr>
2018-11-13 16:46:17 +0100

    first push

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,15 @@
-﻿Test
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
