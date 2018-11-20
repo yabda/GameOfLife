c1c8c4d68be57b91f4173bb5478ad6cd8ecf2ab3
yanis <yanis.abdallah@insa-cvl.fr>
2018-11-20 10:32:20 +0100

    Screen now use conf

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -6,61 +6,19 @@ using GameOfLife.View;
 using GameOfLife.Configuration.Initialisation;
 using GameOfLife.Model;
 using GameOfLife.Configuration.Laws;
+using GameOfLife.Model.Factory;
 
 namespace GameOfLife
 {
     static class Program
     {
-        static void OnClose(object sender, EventArgs e)
-        {
-            RenderWindow window = (RenderWindow)sender;
-            window.Close();
-        }
-         
-        
-
-
         static void Main()
         {
-            // Create the main window
-            RenderWindow app = new RenderWindow(new VideoMode(750, 750), "LA VIE");
-            app.Closed += new EventHandler(OnClose);
-                
-           
-            //init grid
-            int size = 150;
-            Grid g = new Grid(size);
+            Screen app = new Screen();
+            app.Init(new Configuration.Configuration());
 
-            g = InitStratege.Init(new RandomInit(), g);
-
-
+            app.Run();
             
-            //creation of drawable object
-            DrawableObject matrice = Decorator.Decorate(g);
-            Clock c = new Clock();
-
-            // Start the game loop
-            while (app.IsOpen)
-            {
-                // Process events
-                app.DispatchEvents();
-
-                if (c.ElapsedTime.AsMilliseconds() > 100)
-                {
-                    g = LawStratege.Apply(g, new ConwayLaw());
-                    matrice=Decorator.Decorate(g);
-                    c.Restart();
-                }
-                
-                // Clear screen
-                app.Clear();
-
-                app.Draw(matrice);
-                
-
-                // Update the window
-                app.Display();
-            } //End game loop
-        } //End Main()
-    } //End Program
+        } 
+    } 
 }
---
+++ GameOfLife/View/Screen.cs
@@ -0,0 +1,68 @@
+﻿using GameOfLife.Configuration.Initialisation;
+using GameOfLife.Configuration.Laws;
+using GameOfLife.Model;
+using GameOfLife.Model.Factory;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.View
+{
+   class Screen
+    {
+        private RenderWindow App;
+        Grid g;
+        LawStrategy strat;
+        int Speed;
+
+        public void Init(Configuration.Configuration conf)
+        {
+            App = new RenderWindow(new VideoMode(750, 750), "LA VIE");
+            App.Closed += new EventHandler(OnClose);
+
+            g = GridFactory.GetGrid(conf.Size);
+            g = InitStratege.Init(conf.InitStrategy,g);
+            this.strat = conf.LawStrategy;
+            this.Speed = conf.Speed;
+        }
+        
+        static void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
+        public void Run()
+        {
+
+            Clock c = new Clock();
+            DrawableObject matrice = Decorator.Decorate(g);
+
+            while (App.IsOpen)
+            {
+                // Process events
+                App.DispatchEvents();
+
+                if (c.ElapsedTime.AsMilliseconds() > Speed)
+                {
+                    g = LawStratege.Apply(g,strat);
+                    matrice = Decorator.Decorate(g);
+                    c.Restart();
+                }
+
+                // Clear screen
+                App.Clear();
+
+                App.Draw(matrice);
+                // Update the window
+                App.Display();
+            } //End game loop
+        }
+
+    }
+}
