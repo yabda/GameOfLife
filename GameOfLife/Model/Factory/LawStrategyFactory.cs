0a53f456ec7d77c0d4aefc97af461f56495241c8
Selandron <etienne.andrieu@insa-cvl.fr>
2018-11-20 09:50:41 +0100

    Ajout du LawStrategyFactory

---
+++ GameOfLife/Model/Factory/LawStrategyFactory.cs
@@ -0,0 +1,34 @@
+﻿using GameOfLife.Configuration.Laws;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Model.Factory
+{
+    public enum LawType
+    {
+        Alo,
+        Conway,
+        Prof
+    }
+
+    static class LawStrategyFactory
+    {
+        public static LawStrategy GetStrategy(LawType type)
+        {
+            switch (type)
+            {
+                case LawType.Alo:
+                    return new AloLaw();
+                case LawType.Conway:
+                    return new ConwayLaw();
+                case LawType.Prof:
+                    return new ProfLaw();
+                default:
+                    return new ConwayLaw();
+            }
+        }
+    }
+}
